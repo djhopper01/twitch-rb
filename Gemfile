bcfd967b911713bc31ff29d8b5ac82fe95c11a22
Derek Hopper <hopper.derek@gmail.com>
2016-04-11 21:53:47 -0400

    Turns on webmock.

--- Gemfile
+++ Gemfile
@@ -1,2 +1,9 @@
 source 'https://rubygems.org'
+
+gem 'rake'
+
+group :test do
+  gem 'webmock'
+end
+
 gemspec
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,8 @@
 require 'rspec'
 require 'twitch'
+require 'webmock/rspec'
+
+WebMock.disable_net_connect!
 
 RSpec.configure do |config|
   config.color = true
